8942b67565422f952c56b4cf1bcfab1c304cabbe
unknown <fc1396720@gmail.com>
2024-05-31 01:31:07 -1200

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let sidebar = document.querySelector(".nav-links");
+
+
+function sidebarClick() {
+
+    sidebar.classList.toggle('open')
+    // if (sidebar.classList.contains('open')) {
+    //     sidebar.classList.remove('open')
+    // } else{
+    //     sidebar.classList.add('open')
+    // }
+    console.log(sidebar);
+}
